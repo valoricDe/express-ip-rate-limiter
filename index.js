333fed3fda5eada78a191f5b8f60dcefedec8caf
Velten Heyn <github@valoric.de>
2021-10-18 20:02:37 +0200

    Add waiter if some threshold is reached

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-module.exports = (app, threshold = 100) => {
+module.exports = (app, threshold = 100, replenishTimeMs = 10000, backoffThreshold = 0.8 * threshold, waitTime = 5000) => {
   app.set('trust proxy', true)
-  const ips = Object.create(null)
-  return (req, res) => {
-    if(ips[req.ip] > threshold) {
+  const ips = {}
+  return (req, res, next) => {
+    const requestCount = ips[req.ip]
+
+    if(requestCount > threshold) {
       res.status(429).end()
       return
     }
+    if(requestCount > backoffThreshold) {
+      ips[req.ip] = requestCount + 1
+      setTimeout(() => {
+        if((ips[req.ip] -= 1) <= 0) {
+          delete ips[req.ip]
+        }
+      }, replenishTimeMs)
+      setTimeout(next, waitTime)
+      return
+    }
 
-    ips[req.ip] += 1
+    if(!requestCount) ips[req.ip] = 1
+    else ips[req.ip] = requestCount + 1
     setTimeout(() => {
       if((ips[req.ip] -= 1) <= 0) {
         delete ips[req.ip]
       }
-    }, 1000)
+    }, replenishTimeMs)
+    next()
   }
 }
